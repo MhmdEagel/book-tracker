4903bdfb7f3213425d97258c431d0673759a9341
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2024-02-20 09:13:57 +0700

    tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-from django.db import models
+
 
 class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     page = models.IntegerField()
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from main.views import show_main, create_book, show_json, show_xml, show_xml_by_id, show_json_by_id
+from main.views import register
+from main.views import login_user, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('', show_main, name='show_main'),
     path('create-book', create_book, name='create_book'),
     path('json/', show_json, name='show_json'),
@@ -11,5 +16,4 @@ urlpatterns = [
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
 
-    
 ]
